bb94a534d3d003654003df014e76025568486d3a
locnelor <locnelor@icloud.com>
2025-11-27 18:40:03 +0800

    feat(database): 重构数据库模块并更新Prisma客户端配置
    - 删除旧的.gitignore文件
    - 更新Prisma schema格式
    - 修改tsconfig配置以支持ES2023和nodenext模块
    - 更新API控制器以使用新的Prisma客户端
    - 添加生成的Prisma类型和客户端文件
    - 调整package.json配置以直接引用源码

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { prisma } from '@repo/database';
+// import { prisma } from '@app/prisma';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello() {
+    return prisma.sys_user.count()
   }
 }
--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -3,10 +3,17 @@ import { PrismaClient } from "./generated/prisma/client";
 import { prismaClientExtends } from "./extends/autoTimestampAndUUID";
 import { pageExtension } from "./extends/pageExtends";
 export * from "@prisma/client/runtime/client"
-
+import 'dotenv/config'
 export * from "./generated/prisma/client"
 export * from "@prisma/adapter-mariadb";
 
+console.log({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    connectionLimit: 5
+})
 const prismaInstance = new PrismaClient({
     adapter: new PrismaMariaDb({
         host: process.env.DATABASE_HOST,
